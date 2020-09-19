c01d94364f5f9f899acde599a5860a199d76206b
Jacob Kagon <jacob@kagon.net>
2020-09-19 18:37:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,7 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
- updated_pigeon_data = {} 
-  pigeon_data[:color] = "Theo"
-  pigeon_data[:gender] = "Peter Jr."
-  pigeon_data[:lives] = "Lucky"
-  pigeon_data[:"Ms. K"]
-  pigeon_date[:"Queenie"]
-  pigeon_data[:"Andrew"]
-  pigeon_data[:"Alex"]
-  
+ 
+binding.pry  
 end
