318b791dd786ec1dcf36b3e520a6025fb759b095
Jacob Kagon <jacob@kagon.net>
2020-09-19 19:07:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,9 @@ def nyc_pigeon_organizer(data)
  data.each_with_object({}) do |(key, value), final_array|
    value.each do |inner_key, names|
      names.each do |name|
-        if !final_array[name]
-        final_array[name] = {}
-        end
+      if !final_array[name]
+      final_array[name] = {}
+      end
        
   binding.pry
      end
