b87938cfe4ac5d2f1bb211c670494610f9672473
Jacob Kagon <jacob@kagon.net>
2020-09-19 18:27:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
  updated_pigeon_data = {} 
-  pigeon_data[:color] = ":Theo"
-  pige0n_data[:]
+  pigeon_data[:color] = "Theo"
+  pigeon_data[:gender] = "Peter Jr."
+  pigeon_data[:lives] = "Lucky"
+  pigeon_data[:"Ms. K"]
+  pigeon_date[:"Queenie"]
+  pigeon_data[:"Andrew"]
+  pigeon_data[:"Alex"]
   
 end
